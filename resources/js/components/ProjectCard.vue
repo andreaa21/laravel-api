ed500f275ac14370935e05690cbd0520b99366a5
andreaa21 <andrea.zollini@proton.me>
2023-01-27 18:55:06 +0100

    add Project cards

---
+++ resources/js/components/ProjectCard.vue
@@ -0,0 +1,79 @@
+<script>
+export default {
+    name: 'ProjectCard',
+    props:{
+        project: Object
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <h3>{{ project.name }}</h3>
+        <h5 class="client">{{ project.client_name }}</h5>
+        <div class="card-text">
+            <div class="badges">
+
+                <span class="category">{{ project.category.name }}</span>
+
+                <div class="technologies">
+                <ul
+                v-for="item in project.technologies" :key="item.id"
+                > 
+                <li class="technology">{{ item.name}}</li>
+                </ul>
+                </div>
+                
+            </div>
+            <p>{{ project.summary }}</p>
+
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .card{
+        padding: 1rem;
+        // background-color: rgb(209, 209, 209);
+        border: 2px solid rgb(0, 0, 0);
+        border-radius: 20px;
+        width: 300px;
+        height: 400px;
+        overflow: hidden;
+        margin: 1rem;
+        h3{
+            text-transform: uppercase;
+        }
+        .client{
+            margin: 1rem 0;
+        }
+        .badges{
+            margin: 20px 0;
+            display: flex;
+            justify-content: space-between;
+            .technologies{
+                display: flex;
+            }
+            .category{
+            color: white;
+            background-color: rgb(116, 128, 184);
+            padding: .5rem;
+            border-radius: 10px;
+            }
+            ul{
+                list-style: none;
+                margin-top: 10px;
+                display: flex;
+                .technology{
+                    padding: .5rem;
+                    border-radius: 10px;
+                    color: white;
+                    background-color: rgb(66, 141, 91);
+                    font-size: .7rem;
+                    margin: 0 5px;
+                }
+            }
+        }
+    }
+</style>
+
--- resources/js/pages/Projects.vue
+++ resources/js/pages/Projects.vue
@@ -1,15 +1,53 @@
 <script>
+
+import axios from 'axios';
+import ProjectCard from '../components/ProjectCard.vue';
+
+
 export default {
-    name: 'Projects'
+  name: 'Projects',
+  components:{
+    ProjectCard
+  },
+  data(){
+    return{
+      baseUrl: 'http://127.0.0.1:8000/api/',
+      projects: []
+    }
+  },
+  methods:{
+    getApi(){
+      axios.get(this.baseUrl + 'projects')
+          .then(result => {
+            this.projects = result.data.projects;
+            console.log(this.projects);
+          })
+    }
+  },
+  mounted(){
+    this.getApi();
+  }
 }
 </script>
 
 <template>
 <h1>Progetti</h1>
+
+<div class="card-container">
+    <ProjectCard
+        v-for="project in projects" :key="project.id"
+        :project = "project"
+      />
+</div>
 </template>
 
 
 
-<style>
+<style lang="scss" scoped>
 
+    .card-container{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-around;
+    }
 </style>
